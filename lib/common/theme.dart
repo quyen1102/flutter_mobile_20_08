46962072200fda9d1a73fd3601f14786677a8410
quyen1102 <quyen.ttq.1102@gmail.com>
2022-08-21 19:59:35 +0700

    init color theme

---
+++ lib/common/theme.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+Color primarySuperDarkColor = const Color(0xff1e1f26);
+Color primaryDarkColor = const Color(0xff283655);
+Color primaryColor = const Color(0xff4d648d );
+Color primaryLightColor = const Color(0xffd0e1f9 );
+Color primaryWhiteColor = const Color(0xffffffff);
